c62b83ee4974f7fa95f7dbd399ec3256709f293b
Vishvak365 <vishvak365@gmail.com>
2017-05-13 22:51:29 -0400

    modified the name of form

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.BCSN_Football_Scorebug.Form1
+            Me.MainForm = Global.BCSN_Football_Scorebug.Vishvak_Seenichamy_BCSN_FB_SB
         End Sub
     End Class
 End Namespace
